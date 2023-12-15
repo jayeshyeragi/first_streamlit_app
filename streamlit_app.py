137c3f3b6be41e7f1957fc267ff4d81fc014d631
jayeshyeragi <103666657+jayeshyeragi@users.noreply.github.com>
2023-12-14 22:01:07 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,3 +10,7 @@ streamlit.text('✔️ My Text - 3')
 streamlit.text('✔️ My Text - 4')
 
 streamlit.header('❄️ My Header#2 ❄️')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
