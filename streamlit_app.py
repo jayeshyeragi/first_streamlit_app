9448278a683a021bbdec8b1098c8c34338dc0667
jayeshyeragi <103666657+jayeshyeragi@users.noreply.github.com>
2023-12-15 16:16:35 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -14,4 +14,6 @@ streamlit.header('🍍 🥭 🍎 🍏 🍐 🍑 🍒 🍓 My Fruit List #2 🍇 
 
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
+streamlit.multiselect("Pisk some fruits:", list(my_fruit_list.index)
+
 streamlit.dataframe(my_fruit_list)
