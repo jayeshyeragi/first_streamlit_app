38f01fb9bac36c811d8c4aee95e1e34c3ed3da0c
jayeshyeragi <103666657+jayeshyeragi@users.noreply.github.com>
2023-12-14 21:47:44 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,5 +6,6 @@ streamlit.header('My Header#1')
 streamlit.text('My Text - 1')
 streamlit.text('My Text - 2')
 streamlit.text('My Text - 3')
-streamlit.text('My Text - 3')
+streamlit.text('My Text - 4')
 
+streamlit.header('My Header#2')
