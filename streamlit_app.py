ff1efcdef09118074ed65c2dc590c59ec058a823
jayeshyeragi <103666657+jayeshyeragi@users.noreply.github.com>
2023-12-19 18:11:20 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,15 +11,15 @@ streamlit.text('✔️ My Text - 2')
 streamlit.text('✔️ My Text - 3')
 streamlit.text('✔️ My Text - 4')
 
-streamlit.header('🍍 🥭 🍎 🍏 🍐 🍑 🍒 🍓 My Fruit List #2 🍇 🍈 🍉 🍊 🍋 🍌 ');
+streamlit.header('🍍 🥭 🍎 🍏 🍐 🍑 🍒 🍓 My Fruit List #2 🍇 🍈 🍉 🍊 🍋 🍌 ')
 
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt");
-my_fruit_list = my_fruit_list.set_index('Fruit');
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruits_selected = streamlit.multiselect("Pisk some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries']);
-fruits_to_show= my_fruit_list.loc[fruits_selected];
+fruits_selected = streamlit.multiselect("Pisk some fruits:", list(my_fruit_list.index), ['Avocado','Strawberries'])
+fruits_to_show= my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(fruits_to_show);
+fruitvice_response = request.get("https://fruityvice.com/api/fruit/watermelon")
+streamlit.text(fruitvice_response)
 
-fruitvice_response = request.get("https://fruityvice.com/api/fruit/watermelon");
-streamlit.text(fruitvice_response);
+streamlit.dataframe(fruits_to_show)
