258587895eb34cea122e15aade139fb244faeee7
jayeshyeragi <103666657+jayeshyeragi@users.noreply.github.com>
2024-01-11 21:14:28 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -29,13 +29,19 @@ def get_fruityvice_date(this_fruit_choice):
     fruitvice_normalized = pandas.json_normalize(fruitvice_response.json())
     return fruitvice_normalized
 
-
+# new section to display fruityvide api response
+streamlit.header(" Fruityvice Fruit Advice - API ")
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information. ")   
+  else:
+      back_from_function = get_fruityvice_date(fruit_choice)
+      streamlit.dataframe(back_from_function)
   
 
-
-
 #streamlit.header("### Fruityvice ###")
-#streamlit.header("🍒 🍓 Fruityvice Fruit Advice! 🍇 🍈")
+#streamlit.header("🍒 🍓 Fruityvice Fruit Advice! - Errorhandler 🍇 🍈")
 #try:
 #  fruit_choice = streamlit.text_input('What fruit would you like information about?')
 #  if not fruit_choice:
